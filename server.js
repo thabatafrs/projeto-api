1f2497a167eb92ae5ef143329a5f79503e580c28
thabatafrs <thabatafreitas10@gmail.com>
2025-04-14 13:25:40 -0300

    feat: add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import pkg from "@prisma/client";
+import cors from "cors";
 const { PrismaClient } = pkg;
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
